ee4b1fa234566db1dee15f915428b2071ac60e78
ecti <dtworek94@gmail.com>
2025-06-03 23:28:01 +0100

    Add fantasy characters asset, change character appearance and animation. Add hand rotation script

---
+++ Assets/_Project/Scripts/Player/HandRotator.cs
@@ -0,0 +1,33 @@
+using System;
+using KBCore.Refs;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+namespace Explorer
+{
+    public class HandRotator : ValidatedMonoBehaviour
+    {
+        [SerializeField, Self] public Transform hand;
+        [SerializeField] public float radius = 1f;
+
+        private Vector2 _mouseScreenPosition;
+
+        void FixedUpdate()
+        {
+            Vector2 mousePosition = Camera.main.ScreenToWorldPoint(Mouse.current.position.ReadValue());
+            Vector2 direction = (mousePosition - (Vector2)transform.parent.position).normalized;
+
+            transform.position = (Vector2)transform.parent.position + direction * radius;
+
+            float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+            transform.rotation = Quaternion.Euler(0f, 0f, angle);
+
+            hand.localScale = direction.x > 0 ? new Vector3(-1, 1, 1) : new Vector3(-1, -1, 1);
+        }
+
+        public void OnLook(InputAction.CallbackContext context)
+        {
+            _mouseScreenPosition = context.ReadValue<Vector2>();
+        }
+    }
+}
